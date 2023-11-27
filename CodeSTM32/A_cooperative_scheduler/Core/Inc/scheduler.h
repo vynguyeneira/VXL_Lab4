826b480d789e8366dac7a3d3c988d754eb6adfc2
Vy Nguyen <tuyetvynguyen18@gmail.com>
2023-11-27 15:55:51 +0700

    Commit file "scheduler.h"

--- CodeSTM32/A_cooperative_scheduler/Core/Inc/scheduler.h
+++ CodeSTM32/A_cooperative_scheduler/Core/Inc/scheduler.h
@@ -8,6 +8,32 @@
 #ifndef INC_SCHEDULER_H_
 #define INC_SCHEDULER_H_
 
+#include <stdint.h>
+
+#define SCH_MAX_TASKS		40
+
+extern int TIME_CYCLE;
+
+typedef struct
+{
+// Pointer to the task (must be a ’ void ( void ) ’ function )
+void ( * pTask ) ( void ) ;
+// Delay ( ticks ) until the function will ( next ) be run
+uint32_t Delay ;
+// Interval ( ticks ) between subsequent runs .
+uint32_t Period ;
+// Incremented (by scheduler) when task i s due to execute
+uint8_t RunMe;
+//This i s a hint to solve the question below .
+uint32_t TaskID ;
+} sTask ;
+
+
+void SCH_int(void);
+uint32_t SCH_Add_Task (void (*pFunction)(),
+			uint32_t DELAY, uint32_t PERIOD);
+void SCH_Update(void);
+void SCH_Dispatch_Tasks(void);
 
 
 #endif /* INC_SCHEDULER_H_ */
